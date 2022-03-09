66b9f23d5ea2345954149f44f1f5fcc0572499d6
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2022-03-08 22:17:19 -0500

    Use storybook

---
+++ src/components/AutoTextbox/AutoTextbox.stories.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { ComponentMeta } from "@storybook/react";
+
+import AutoTextbox from ".";
+
+export default {
+    title: "Example/AutoTextbox",
+    component: AutoTextbox,
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
+} as ComponentMeta<typeof AutoTextbox>;
+
+export const Primary = () => {
+    // Sets the hooks for both the label and primary props
+    const [animate, setAnimate] = useState(false);
+    const [reset, setReset] = useState(false);
+
+    return (
+        <>
+            <AutoTextbox
+                animate={animate}
+                value={"testing"}
+                reset={reset}
+                onReset={() => setReset(false)}
+                onFinish={() => setAnimate(false)}
+                textbox={<input type="text" />}
+            />
+            <button type="button" onClick={() => setAnimate(true)}>
+                Animate
+            </button>
+            <button
+                type="button"
+                onClick={() => {
+                    setReset(true);
+                    setAnimate(false);
+                }}
+            >
+                Reset
+            </button>
+        </>
+    );
+};
