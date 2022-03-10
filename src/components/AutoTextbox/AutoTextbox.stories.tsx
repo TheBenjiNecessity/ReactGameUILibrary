7f3d30f8a425922e603ce9a99d87fc0057479152
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2022-03-10 17:27:32 -0500

    Update stories

--- src/components/AutoTextbox/AutoTextbox.stories.tsx
+++ src/components/AutoTextbox/AutoTextbox.stories.tsx
@@ -41,3 +41,5 @@ export const Primary = () => {
         </>
     );
 };
+
+Primary.storyName = "AutoTextbox";
--- src/components/Dice/Dice/index.tsx
+++ src/components/Dice/Dice/index.tsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import Shape, { SHAPE } from '../../Shape';
+import React from "react";
+import Shape, { SHAPE } from "../../Shape";
 
 export const DIE_COLOR = {
-	WHITE: '#ffffff',
-	BLACK: '#000000',
-	RED: '#ff0000',
-	GREEN: '#00ff00',
-	BLUE: '#0000ff',
-	YELLOW: '#ffff00',
-	ORANGE: '#FFA500',
-	PURPLE: '#800080'
+    WHITE: "#ffffff",
+    BLACK: "#000000",
+    RED: "#ff0000",
+    GREEN: "#00ff00",
+    BLUE: "#0000ff",
+    YELLOW: "#ffff00",
+    ORANGE: "#FFA500",
+    PURPLE: "#800080",
 };
 
 type DieProps = {
-	children: object;
-	size: number;
-	color: string;
-	shape: SHAPE;
+    children: object;
+    size: number;
+    color: string;
+    shape: SHAPE;
 };
 
 const Dice = (props: DieProps) => {
-	const { children, size, color, shape } = props;
+    const { children, size, color, shape } = props;
 
-	return (
-		<Shape size={size} color={color} shape={shape}>
-			{children}
-		</Shape>
-	);
+    return (
+        <Shape size={size} color={color} shape={shape}>
+            {children}
+        </Shape>
+    );
 };
 
 export default Dice;
@@ -38,7 +38,7 @@ export default Dice;
  *      Pip Die
  *      Image Die
  *          Sprite Die
- * 
+ *
  * Dice can have different:
  *      - shapes
  *      - numbers vs pips vs images vs strings
