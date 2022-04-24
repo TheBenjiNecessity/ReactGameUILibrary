bfaf6eea5e63c864863e8063fa27a0ddda0c1eba
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2022-04-24 14:36:23 -0400

    Add useMovementArray hook

---
+++ src/components/Animations/AnimateObject/useMovementArray.hook.ts
@@ -0,0 +1,31 @@
+import { useMemo } from "react";
+import { IKeyframe } from "./AnimateObject";
+import { getKeyFrame } from "./keyFrame.service";
+
+const useMovementArray = (keyframeList: IKeyframe[], loop: boolean) => {
+    return useMemo(() => {
+        if (!keyframeList || !keyframeList.length) {
+            return [];
+        }
+
+        return keyframeList.map((keyframe, index) => {
+            if (index === keyframeList.length - 1) {
+                if (loop) {
+                    const fromKeyframe = keyframeList[index];
+                    const toKeyframe = keyframeList[0];
+
+                    return getKeyFrame(fromKeyframe, toKeyframe);
+                } else {
+                    return null;
+                }
+            }
+
+            const fromKeyframe = keyframe;
+            const toKeyframe = keyframeList[index + 1];
+
+            return getKeyFrame(fromKeyframe, toKeyframe);
+        });
+    }, [keyframeList, loop]);
+};
+
+export default useMovementArray;
