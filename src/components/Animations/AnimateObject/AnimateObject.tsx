2a9248ee5913d7a38e16285614d324c7f2c35f81
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2022-04-24 14:36:47 -0400

    Code cleanup

--- src/components/Animations/AnimateObject/AnimateObject.tsx
+++ src/components/Animations/AnimateObject/AnimateObject.tsx
@@ -1,8 +1,8 @@
-import { useMemo, useEffect, HTMLAttributes, useState } from "react";
+import { useEffect, HTMLAttributes, useState } from "react";
 import Point from "../../../lib/Geometry/Point";
 import AnimateProps from "../AnimateProps.type";
-import { getAnimationComponent, getKeyFrame } from "./keyFrame.service";
-import usePlayState from "../usePlayState.hook";
+import { getAnimationComponent } from "./keyFrame.service";
+import useMovementArray from "./useMovementArray.hook";
 
 export type IKeyframe = {
     point: Point;
@@ -30,54 +30,24 @@ const AnimateObject = (
         count,
         children,
     } = props;
-
-    const playState = usePlayState(play);
+    const { time, easing, point } = keyframeList[keyframeIndex];
+    const playState = play ? "running" : "paused";
 
     const [currentCount, setCurrentCount] = useState(count);
-
-    const movementArray = useMemo(() => {
-        if (!keyframeList || !keyframeList.length) {
-            return [];
-        }
-
-        return keyframeList.map((keyframe, index) => {
-            if (index === keyframeList.length - 1) {
-                if (loop) {
-                    const fromKeyframe = keyframeList[index];
-                    const toKeyframe = keyframeList[0];
-
-                    return getKeyFrame(fromKeyframe, toKeyframe);
-                } else {
-                    return null;
-                }
-            }
-
-            const fromKeyframe = keyframe;
-            const toKeyframe = keyframeList[index + 1];
-
-            return getKeyFrame(fromKeyframe, toKeyframe);
-        });
-    }, [keyframeList, loop]);
-
-    const currentKeyframe = useMemo(() => {
-        return movementArray[keyframeIndex];
-    }, [keyframeIndex, movementArray]);
-
-    const { time, easing, point } = useMemo(() => {
-        return keyframeList[keyframeIndex];
-    }, [keyframeIndex, keyframeList]);
+    const movementArray = useMovementArray(keyframeList, loop);
 
     const Component = getAnimationComponent(
         point,
-        currentKeyframe,
+        movementArray[keyframeIndex],
         time,
         easing,
         playState
     );
 
     useEffect(() => {
+        let timeout: number;
         if (play) {
-            setTimeout(() => {
+            timeout = window.setTimeout(() => {
                 if (keyframeIndex + 1 < keyframeList.length) {
                     setKeyframeIndex(keyframeIndex + 1);
                 } else if (loop) {
@@ -88,6 +58,10 @@ const AnimateObject = (
                 }
             }, time * 1000);
         }
+
+        return () => {
+            clearTimeout(timeout);
+        };
     }, [
         count,
         currentCount,
