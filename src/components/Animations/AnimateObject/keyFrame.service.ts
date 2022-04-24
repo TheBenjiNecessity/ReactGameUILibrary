b999013c9aa0afb27839bc71c550281fe99471ce
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2022-04-23 22:53:03 -0400

    Add getAnimationComponent method

--- src/components/Animations/AnimateObject/keyFrame.service.ts
+++ src/components/Animations/AnimateObject/keyFrame.service.ts
@@ -1,4 +1,5 @@
-import { keyframes } from "styled-components";
+import styled, { Keyframes, keyframes } from "styled-components";
+import Point from "../../../lib/Geometry/Point";
 import { IKeyframe } from "./AnimateObject";
 
 export const getKeyFrame = (fromKeyframe: IKeyframe, toKeyframe: IKeyframe) => {
@@ -11,5 +12,22 @@ export const getKeyFrame = (fromKeyframe: IKeyframe, toKeyframe: IKeyframe) => {
             left: ${toKeyframe.point.x}px;
             top: ${toKeyframe.point.y}px;
         }
+    `;
+};
+
+export const getAnimationComponent = (
+    point: Point,
+    currentKeyframe: Keyframes | null,
+    time: number,
+    easing: string,
+    playState: string
+) => {
+    return styled.div`
+        display: inline-block;
+        position: absolute;
+        top: ${point.y}px;
+        left: ${point.x}px;
+        animation: ${currentKeyframe} ${time}s ${easing};
+        animation-play-state: ${playState};
     `;
 };
