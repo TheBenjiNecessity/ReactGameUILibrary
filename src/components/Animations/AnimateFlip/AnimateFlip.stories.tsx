bb507e281e253428dba38162597cf3a8a92766b0
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2022-05-10 22:36:15 -0400

    Add ability to set flip axis

--- src/components/Animations/AnimateFlip/AnimateFlip.stories.tsx
+++ src/components/Animations/AnimateFlip/AnimateFlip.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { ComponentMeta } from "@storybook/react";
-import AnimateFlip from "./AnimateFlip";
+import AnimateFlip, { FlipAxis } from "./AnimateFlip";
 
 export default {
     title: "AnimateFlip",
@@ -49,3 +49,46 @@ Primary.args = {
 };
 
 Primary.storyName = "AnimateFlip";
+
+export const Secondary = (args: any) => {
+    const [flip, setFlip] = useState(false);
+
+    const cardStyles = {
+        height: "300px",
+        width: "200px",
+    };
+
+    const cardStylesInner = {
+        height: "100%",
+        width: "100%",
+    };
+
+    return (
+        <>
+            <button onClick={() => setFlip(!flip)}>flip</button>
+            <AnimateFlip
+                flip={flip}
+                back={
+                    <div style={{ ...cardStylesInner, backgroundColor: "red" }}>
+                        blah
+                    </div>
+                }
+                use3D={true}
+                flipSpeed={0.8}
+                flipAxis={FlipAxis.X}
+                style={cardStyles}
+                didFlip={(flipped: boolean) => console.log("did flip", flipped)}
+            >
+                <div style={{ ...cardStylesInner, backgroundColor: "blue" }}>
+                    test
+                </div>
+            </AnimateFlip>
+        </>
+    );
+};
+
+Secondary.args = {
+    play: true,
+};
+
+Secondary.storyName = "AnimateFlipX";
--- src/components/Animations/AnimateFlip/AnimateFlip.tsx
+++ src/components/Animations/AnimateFlip/AnimateFlip.tsx
@@ -3,12 +3,18 @@ import clsx from "clsx";
 import styled from "styled-components";
 import classes from "./AnimateFlip.module.scss";
 
+export enum FlipAxis {
+    X = 1,
+    Y,
+}
+
 type AnimateFlipProps = {
     flip: boolean;
     back: ReactNode;
     use3D: boolean;
     flipSpeed: number;
     didFlip: Function;
+    flipAxis?: FlipAxis;
 };
 
 type InnerDivStyle = {
@@ -30,12 +36,31 @@ const FlipContainerOuter = styled.div`
 const AnimateFlip = (
     props: AnimateFlipProps & HTMLAttributes<HTMLDivElement>
 ) => {
-    const { use3D, flip, flipSpeed, back, didFlip, children, ...restProps } =
-        props;
+    const {
+        use3D,
+        flip,
+        flipSpeed,
+        flipAxis,
+        back,
+        didFlip,
+        children,
+        ...restProps
+    } = props;
 
     const innerClassNames = clsx(
         classes["flip-container-inner"],
-        flip && classes["flipped"]
+        flip && classes["flipped"],
+        flipAxis === FlipAxis.X ? classes["X"] : classes["Y"]
+    );
+
+    const frontClassNames = clsx(
+        classes["flip-container-front"],
+        flipAxis === FlipAxis.X ? classes["X"] : classes["Y"]
+    );
+
+    const backClassNames = clsx(
+        classes["flip-container-back"],
+        flipAxis === FlipAxis.X ? classes["X"] : classes["Y"]
     );
 
     return (
@@ -49,10 +74,8 @@ const AnimateFlip = (
                 className={innerClassNames}
                 onTransitionEnd={(event) => didFlip(flip, event)}
             >
-                <div className={classes["flip-container-front"]}>
-                    {children}
-                </div>
-                <div className={classes["flip-container-back"]}>{back}</div>
+                <div className={frontClassNames}>{children}</div>
+                <div className={backClassNames}>{back}</div>
             </FlipContainerInner>
         </FlipContainerOuter>
     );
