dab9eedbce695fbf33cd5e620ac780bd7389b1af
TheBenjiNecessity <TheBenjiNecessity@users.noreply.github.com>
2022-05-29 22:52:00 -0400

    Add more tests

--- src/components/Animations/AnimateWiggle/AnimateWiggle.stories.tsx
+++ src/components/Animations/AnimateWiggle/AnimateWiggle.stories.tsx
@@ -19,17 +19,50 @@ export const Primary = (args: any) => {
     return (
         <>
             <button onClick={() => setPlay(!play)}>{playMessage}</button>
-            <AnimateWiggle
-                play={play}
-                count={0}
-                axis={Axis.X}
-                easing="linear"
-                time={0.1}
-                offset={5}
-                onAnimationEnd={() => console.log("On AnimateWiggle End")}
-            >
-                test
-            </AnimateWiggle>
+            <p>
+                <AnimateWiggle
+                    play={play}
+                    count={5}
+                    axis={Axis.X}
+                    easing="linear"
+                    time={0.1}
+                    offset={5}
+                    onAnimationEnd={() => console.log("On AnimateWiggle End")}
+                    style={{ display: "inline-block" }}
+                >
+                    <span
+                        style={{ border: "1px solid black", padding: "0 5px" }}
+                    >
+                        test
+                    </span>
+                </AnimateWiggle>
+            </p>
+
+            <p>
+                <AnimateWiggle
+                    play={true}
+                    count={0}
+                    axis={Axis.X}
+                    easing="linear"
+                    time={0.5}
+                    offset={5}
+                >
+                    x axis
+                </AnimateWiggle>
+            </p>
+
+            <p>
+                <AnimateWiggle
+                    play={true}
+                    count={0}
+                    axis={Axis.Y}
+                    easing="linear"
+                    time={0.5}
+                    offset={5}
+                >
+                    y axis
+                </AnimateWiggle>
+            </p>
         </>
     );
 };
